f810001f05ba6c444041f4bc300426064d87d812
Chamillion <chamillionog@gmail.com>
2026-08-22 22:45:46 -0400

    Made About Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Header from "./components/Header.jsx"
+import About from "./components/About.jsx"
 
 export default function App() {
   return (
     <>
       <Header />
+      <About />
     </>
   )
 }
---
+++ src/components/About.jsx
@@ -0,0 +1,25 @@
+export default function About() {
+  return (
+    <section id="about" className="about">
+
+      <div className="about-content">
+        <span className="category">ABOUT ME</span>
+
+        <h1>Developer.<br /><span>Creator.</span></h1>
+
+        <p className="about-description">
+          I'm a programmer and game developer who enjoys building
+          interactive experiences and bringing ideas to life through code.
+        </p>
+      </div>
+
+      <div className="about-image">
+        <img
+          src="/PortfolioPicture.jpg"
+          alt="Portfolio"
+        />
+      </div>
+
+    </section>
+  );
+}

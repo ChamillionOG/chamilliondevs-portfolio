dc069d78abe336733d13a0a1d863b7857f3b1e2e
Chamillion <chamillionog@gmail.com>
2026-08-24 20:16:33 -0400

    Added 2 more projects and finished EDS card

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -19,7 +19,7 @@ export default function Projects() {
                             {/*Info*/}
                             <div className="title-container">
                                 <div className="project-title">Exploding Delivery Service</div>
-                                <div className="project-role">Programmer · Animator · UI Designer · Marketer</div>
+                                <div className="project-role">Programmer · Animator · UI Designer · Marketer · C++ · Blueprints</div>
                             </div>
                         </div>
 
@@ -43,10 +43,95 @@ export default function Projects() {
                         {/*Images*/}
                         <div className="project-gallery">
                             <div className="project-image"><img src="../public/EDSImages/SS1.png" onClick={() => setSelectedImage("../public/EDSImages/SS1.png")} /></div>
+                            <div className="project-image"><img src="../public/EDSImages/SS2.png" onClick={() => setSelectedImage("../public/EDSImages/SS2.png")} /></div>
+                            <div className="project-image"><img src="../public/EDSImages/SS3.png" onClick={() => setSelectedImage("../public/EDSImages/SS3.png")} /></div>
+                            <div className="project-image"><img src="../public/EDSImages/SS4.png" onClick={() => setSelectedImage("../public/EDSImages/SS4.png")} /></div>
+                            <div className="project-image"><img src="../public/EDSImages/SS5.png" onClick={() => setSelectedImage("../public/EDSImages/SS5.png")} /></div>
+                            <div className="project-image"><img src="../public/EDSImages/SS6.png" onClick={() => setSelectedImage("../public/EDSImages/SS6.png")} /></div>
+                            <div className="project-image"><img src="../public/EDSImages/SS7.png" onClick={() => setSelectedImage("../public/EDSImages/SS7.png")} /></div>
+                            <div className="project-image"><img src="../public/EDSImages/SS8.png" onClick={() => setSelectedImage("../public/EDSImages/SS8.png")} /></div>
+                            <div className="project-image"><img src="../public/EDSImages/SS9.png" onClick={() => setSelectedImage("../public/EDSImages/SS9.png")} /></div>
                         </div>
                     </div>
 
+                    {/* Insectrix */}
+                    <div className="project-card">
+                        <div className="project-header">
+                            <div className="project-icon"><img src="../public/InsectrixImages/Icon.png" /></div>
+                            
+                            {/*Info*/}
+                            <div className="title-container">
+                                <div className="project-title">Insectrix</div>
+                                <div className="project-role">Fully Self Developed · Python</div>
+                            </div>
+                        </div>
 
+                        {/*Status*/}
+                        <div className="status">
+                            <div className="unreleased"></div>
+                            <span className="status-text">Unreleased</span>
+                        </div>
+
+                        {/*Description*/}
+                        <div className="project-description">
+                                Insectrix is a bug-collecting adventure where you explore new environments, catch unique insects, and build your collection. Sell bugs to earn insectra, upgrade your equipment, and unlock new areas as you progress.
+                        </div>
+
+                        {/*Images*/}
+                        <div className="project-gallery">
+                            <div className="project-image"><img src="../public/InsectrixImages/SS1.png" onClick={() => setSelectedImage("../public/InsectrixImages/SS1.png")} /></div>
+                            <div className="project-image"><img src="../public/InsectrixImages/SS2.png" onClick={() => setSelectedImage("../public/InsectrixImages/SS2.png")} /></div>
+                            <div className="project-image"><img src="../public/InsectrixImages/SS3.png" onClick={() => setSelectedImage("../public/InsectrixImages/SS3.png")} /></div>
+                            <div className="project-image"><img src="../public/InsectrixImages/SS4.png" onClick={() => setSelectedImage("../public/InsectrixImages/SS4.png")} /></div>
+                            <div className="project-image"><img src="../public/InsectrixImages/SS5.png" onClick={() => setSelectedImage("../public/InsectrixImages/SS5.png")} /></div>
+                            <div className="project-image"><img src="../public/InsectrixImages/SS6.png" onClick={() => setSelectedImage("../public/InsectrixImages/SS6.png")} /></div>
+                            <div className="project-image"><img src="../public/InsectrixImages/SS7.png" onClick={() => setSelectedImage("../public/InsectrixImages/SS7.png")} /></div>
+                            <div className="project-image"><img src="../public/InsectrixImages/SS8.png" onClick={() => setSelectedImage("../public/InsectrixImages/SS8.png")} /></div>
+                            <div className="project-image"><img src="../public/InsectrixImages/SS9.png" onClick={() => setSelectedImage("../public/InsectrixImages/SS9.png")} /></div>
+                        </div>
+                    </div>
+
+                    {/* Autophobia */}
+                    <div className="project-card">
+                        <div className="project-header">
+                            <div className="project-icon"><img src="../public/AutophobiaImages/Icon.png" /></div>
+                            
+                            {/*Info*/}
+                            <div className="title-container">
+                                <div className="project-title">Autophobia</div>
+                                <div className="project-role">Animator · Part-Scripter · UI · Lua</div>
+                            </div>
+                        </div>
+
+                        {/*Link*/}
+                        <a href="https://www.roblox.com/games/100280838747749/Autophobia" className="project-link" target="blank" rel="noreferrer"> 
+                            View Project
+                            <span>↗</span>
+                        </a>
+
+                        {/*Status*/}
+                        <div className="status">
+                            <div className="released"></div>
+                            <span className="status-text">Released</span>
+                        </div>
+
+                        {/*Description*/}
+                        <div className="project-description">
+                            Backrooms inspired Roblox game with custom entities, gameplay, levels, and more.
+                        </div>
+
+                        {/*Images*/}
+                        <div className="project-gallery">
+                            <div className="project-image"><img src="../public/AutophobiaImages/SS1.png" onClick={() => setSelectedImage("../public/AutophobiaImages/SS1.png")} /></div>
+                            <div className="project-image"><img src="../public/AutophobiaImages/SS2.png" onClick={() => setSelectedImage("../public/AutophobiaImages/SS2.png")} /></div>
+                            <div className="project-image"><img src="../public/AutophobiaImages/SS3.png" onClick={() => setSelectedImage("../public/AutophobiaImages/SS3.png")} /></div>
+                            <div className="project-image"><img src="../public/AutophobiaImages/SS4.png" onClick={() => setSelectedImage("../public/AutophobiaImages/SS4.png")} /></div>
+                            <div className="project-image"><img src="../public/AutophobiaImages/SS5.png" onClick={() => setSelectedImage("../public/AutophobiaImages/SS5.png")} /></div>
+                            <div className="project-image"><img src="../public/AutophobiaImages/SS6.png" onClick={() => setSelectedImage("../public/AutophobiaImages/SS6.png")} /></div>
+                            <div className="project-image"><img src="../public/AutophobiaImages/SS7.png" onClick={() => setSelectedImage("../public/AutophobiaImages/SS7.png")} /></div>
+                            <div className="project-image"><img src="../public/AutophobiaImages/SS8.png" onClick={() => setSelectedImage("../public/AutophobiaImages/SS8.png")} /></div>
+                        </div>
+                    </div>
 
                 </div>
             </div>
